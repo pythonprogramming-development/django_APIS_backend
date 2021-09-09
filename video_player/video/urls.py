42bc2a83065c0b2a5eaefd5473edd0867cbcca20
pranay77738 <pranay.ravula7@gmail.com>
2021-09-09 10:45:08 +0200

    commit

--- video_player/video/urls.py
+++ video_player/video/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import HistoryListView, HistoryAddedView, BookmarkAddedView, BookmarkListView
+from .views import HistoryAddedView, history_list, BookmarkAddedView, bookmark_list
 
 urlpatterns = [
     path('', HistoryAddedView.as_view(), name='history-add'),
-    path('history/list/', HistoryListView.as_view(), name='history-list'),
-    path('bookmark/list/', BookmarkListView.as_view(), name='bookmark-list'),
+    path('history/list/', history_list, name='history-list'),
+    path('bookmark/list/', bookmark_list, name='bookmark-list'),
     path('bookmark/add/', BookmarkAddedView.as_view(), name='bookmark-add'),
 ]
--- video_player/video/views.py
+++ video_player/video/views.py
@@ -2,14 +2,8 @@ from .models import History, Bookmark
 from django.views.generic import ListView, CreateView
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-
-
-@method_decorator(csrf_exempt, name='dispatch')
-class HistoryListView(ListView):
-    model = History
-    template_name = 'video/history.html'  # <app>/<model>_<viewtype>.html
-    context_object_name = 'objects'
-    ordering = ['-date_watched']
+from django.http import HttpResponse
+from django.core.serializers import serialize
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -22,12 +16,10 @@ class HistoryAddedView(CreateView):
         return super().form_valid(form)
 
 
-@method_decorator(csrf_exempt, name='dispatch')
-class BookmarkListView(ListView):
-    model = Bookmark
-    template_name = 'video/bookmarks.html'  # <app>/<model>_<viewtype>.html
-    context_object_name = 'posts'
-    ordering = ['-date_added']
+def history_list(request):
+    obj = History.objects.all().order_by('-date_watched')
+    data = serialize("json", obj, fields=('history_url'))
+    return HttpResponse(data, content_type="application/json")
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -38,3 +30,24 @@ class BookmarkAddedView(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+
+def bookmark_list(request):
+    obj = Bookmark.objects.all().order_by('-date_added')
+    data = serialize("json", obj, fields=('bookmark_url'))
+    return HttpResponse(data, content_type="application/json")
+
+# @method_decorator(csrf_exempt, name='dispatch')
+# class HistoryListView(ListView):
+#     model = History
+#     template_name = 'video/history.html'  # <app>/<model>_<viewtype>.html
+#     context_object_name = 'objects'
+#     ordering = ['-date_watched']
+
+
+# @method_decorator(csrf_exempt, name='dispatch')
+# class BookmarkListView(ListView):
+#     model = Bookmark
+#     template_name = 'video/bookmarks.html'  # <app>/<model>_<viewtype>.html
+#     context_object_name = 'posts'
+#     ordering = ['-date_added']
